8e24bc666c14a329d255d5ed5426482709f382f6
Polichca <Polina_glubokova@mail.ru>
2024-05-29 14:28:03 +0500

    fix cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,14 +1,12 @@
 const allowedCors = [
-  "https://practicum.yandex.ru",
-  "https://students-projects.ru",
-  "localhost:3001",
+  "https://pindie-frontend-top.nomorepartiesco.ru",
+  "localhost:3000",
 ];
 
 function cors(req, res, next) {
   const { origin } = req.headers;
 
   if (allowedCors.includes(origin)) {
-    // Если это наш друг
     res.header("Access-Control-Allow-Origin", origin);
     res.header(
       "Access-Control-Allow-Methods",
